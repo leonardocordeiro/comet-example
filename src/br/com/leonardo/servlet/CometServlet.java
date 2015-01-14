eff01a9927299c5fbc2e3c3c4e0ad8892387d6b0
Leonardo B. Cordeiro <leonardo.b.cordeiro@ig.com.br>
2015-01-14 00:19:04 -0200

    Update CometServlet.java

--- src/br/com/leonardo/servlet/CometServlet.java
+++ src/br/com/leonardo/servlet/CometServlet.java
@@ -18,81 +18,83 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns={"/comet"}, asyncSupported=true)
 public class CometServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 	
+	private Queue<AsyncContext> clientes = new ConcurrentLinkedQueue<>();
 	private BlockingQueue<String> mensagens = new LinkedBlockingQueue<>();
-	private ConcurrentLinkedQueue<AsyncContext> clientes = new ConcurrentLinkedQueue<>();
 	
 	@Override
 	public void init() throws ServletException {
-		Executors.newSingleThreadExecutor().execute(new Runnable() {
-			
-			@Override
+		Executors.newSingleThreadExecutor().submit(new Runnable() {
 			public void run() {
 				while(true) {
 					try {
+						//bloqueia a thread até entrar alguma mensagem na fila
 						String mensagem = mensagens.take();
 						enviar(mensagem);
-					} catch(InterruptedException e) {
-						throw new RuntimeException(e);
+					} catch(InterruptedException e) { 
+						e.printStackTrace();
 					}
 				}
 			}
-
 		});
 	}
-
+	
 	private void enviar(String mensagem) {
-		for (AsyncContext ctx : clientes) {
-			try {
-				HttpServletResponse res = (HttpServletResponse) ctx.getResponse();
-				res.setContentType("text/html");
-				
-				PrintWriter writer = res.getWriter();
-				writer.println(mensagem + "<br>");
-				writer.flush();
-				
-				writer.close();
-				ctx.complete();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+		for(AsyncContext cliente : clientes) { 
+			enviar(cliente, mensagem);
 		}
 	}
 	
+	private void enviar(AsyncContext cliente, String mensagem) {
+		HttpServletResponse response = (HttpServletResponse) cliente.getResponse();
+		response.setHeader("content-type", "text/html; charset=UTF-8");
+		try {
+			PrintWriter writer = response.getWriter();
+			writer.println(mensagem);
+			writer.flush();
+		
+			// delegar uma thread para responder ao cliente
+			// ao chamar complete, o cliente sairá da fila
+			cliente.complete();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} 
+	}
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		AsyncContext ctx = req.startAsync();
-		ctx.setTimeout(30000);
-		ctx.addListener(new AsyncListener() {
-			
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		AsyncContext cliente = req.startAsync();
+		cliente.setTimeout(3000);
+		cliente.addListener(new AsyncListener() {
 			@Override
-			public void onTimeout(AsyncEvent arg0) throws IOException {
-				clientes.remove(arg0.getAsyncContext());
+			public void onComplete(AsyncEvent event) throws IOException {
+				clientes.remove(event.getAsyncContext());
 			}
-			
+
 			@Override
-			public void onStartAsync(AsyncEvent arg0) throws IOException {
+			public void onError(AsyncEvent event) throws IOException {
+				event.getAsyncContext().complete();
+				clientes.remove(event.getAsyncContext());
 			}
-			
+
 			@Override
-			public void onError(AsyncEvent arg0) throws IOException {
-				clientes.remove(arg0.getAsyncContext());
+			public void onStartAsync(AsyncEvent event) throws IOException {
 			}
-			
+
 			@Override
-			public void onComplete(AsyncEvent arg0) throws IOException {
-				clientes.remove(arg0.getAsyncContext());				
+			public void onTimeout(AsyncEvent event) throws IOException {
+				event.getAsyncContext().complete();
+				clientes.remove(event.getAsyncContext());
 			}
 		});
-		clientes.add(ctx);
 		
+		clientes.add(cliente);
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String mensagem = req.getParameter("mensagem");
 		mensagens.add(mensagem);
 	}
